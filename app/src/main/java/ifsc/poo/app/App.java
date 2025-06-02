d33f93084f0932feb371b2ea81a6335ca9e069a9
disfoggia <143456100+disfoggia@users.noreply.github.com>
2025-06-01 22:40:03 -0300

    update App.java

--- app/src/main/java/ifsc/poo/app/App.java
+++ app/src/main/java/ifsc/poo/app/App.java
@@ -11,11 +11,11 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
-            System.out.println("\n=== CATÁLOGO DE FILMES ===");
+            System.out.println("\n=== CATALOGO DE FILMES ===");
             System.out.println("1. Adicionar um filme");
             System.out.println("2. Remover um filme");
-            System.out.println("3. Listar por título");
-            System.out.println("4. Listar por gênero");
+            System.out.println("3. Listar por titulo");
+            System.out.println("4. Listar por genero");
             System.out.println("5. Listar por ano");
             System.out.println("6. Buscar por ano");
             System.out.println("0. Sair");
@@ -34,18 +34,18 @@ public class App {
                 case 2 -> removerFilme(catalogo, scanner);
                 case 3, 4, 5 -> listarFilmes(catalogo, opcao);
                 case 6 -> buscarPorAno(catalogo, scanner);
-                default -> System.out.println("Opção inválida!");
+                default -> System.out.println("Opcao invalida!");
             }
         }
     }
 
     private static void adicionarFilme(CatalogoFilmes catalogo, Scanner scanner) {
-        System.out.print("Título que gostaria de adicionar: ");
+        System.out.print("Titulo que gostaria de adicionar: ");
         String titulo = scanner.nextLine();
         System.out.print("Ano: ");
         int ano = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("Gênero: ");
+        System.out.print("Genero: ");
         String genero = scanner.nextLine();
         
         catalogo.adicionarFilme(new Filme(titulo, ano, genero));
@@ -53,7 +53,7 @@ public class App {
     }
 
     private static void removerFilme(CatalogoFilmes catalogo, Scanner scanner) {
-        System.out.print("Título que gostaria de remover: ");
+        System.out.print("Titulo que gostaria de remover: ");
         String titulo = scanner.nextLine();
         catalogo.removerFilme(titulo);
         System.out.println("Filme removido!");

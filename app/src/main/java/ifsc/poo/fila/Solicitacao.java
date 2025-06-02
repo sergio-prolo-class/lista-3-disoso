63255c037bb6cdb2b441001a734bcc6de821b622
disfoggia <143456100+disfoggia@users.noreply.github.com>
2025-06-01 23:13:42 -0300

    criando Solicitacao.java

--- app/src/main/java/ifsc/poo/fila/Solicitacao.java
+++ app/src/main/java/ifsc/poo/fila/Solicitacao.java
@@ -1,4 +1,15 @@
 package ifsc.poo.fila;
 
 public class Solicitacao {
+    private Cliente cliente;
+    private String descricao;
+    
+    public Solicitacao(Cliente cliente, String descricao) {
+        this.cliente = cliente;
+        this.descricao = descricao;
+    }
+    
+    // Getters
+    public Cliente getCliente() { return cliente; }
+    public String getDescricao() { return descricao; }
 }

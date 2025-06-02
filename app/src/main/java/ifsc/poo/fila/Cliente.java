208bb29424320d93bf104387bcfe45c454ae964a
disfoggia <143456100+disfoggia@users.noreply.github.com>
2025-06-01 23:13:17 -0300

    criando Cliente.java

--- app/src/main/java/ifsc/poo/fila/Cliente.java
+++ app/src/main/java/ifsc/poo/fila/Cliente.java
@@ -1,4 +1,18 @@
 package ifsc.poo.fila;
 
 public class Cliente {
+    private String nome;
+    private int idade;
+    private String telefone;
+    
+    public Cliente(String nome, int idade, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+    
+    // Getters
+    public String getNome() { return nome; }
+    public int getIdade() { return idade; }
+    public String getTelefone() { return telefone; }
 }
